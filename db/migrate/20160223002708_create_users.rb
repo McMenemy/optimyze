3d4b8778eb0239821e4fd6bfc789d780f9cda4c4
McMenemy <jtm35@duke.edu>
2016-02-22 18:23:27 -0800

    change migration for heroku

--- db/migrate/20160223002708_create_users.rb
+++ db/migrate/20160223002708_create_users.rb
@@ -1,6 +1,5 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    drop_table :users
     create_table :users do |t|
       t.string :username, null: false
       t.string :password_digest, null: false
